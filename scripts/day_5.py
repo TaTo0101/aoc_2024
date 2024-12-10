7349cdd458d15d4fbd4f00e33c5e74136fe75bb8
agottschalg <extern.antonio.gottschalg@aldi-sued.com>
2024-12-10 15:34:58 +0100

    finished day 5

--- scripts/day_5.py
+++ scripts/day_5.py
@@ -1,4 +1,4 @@
-import numpy as np
+"""Script for solving day 5 of aoc"""
 
 # Initialize empty lists to store both rules and sequences
 raw_data = []
@@ -40,7 +40,7 @@ print(rules_dict[24])
 correct_sequences = []
 incorrect_sequences = []
 for seq in sequences:
-    violates = False
+    VIOLATES = False
     # to keep track which vals where already checked
     vals_checked = set(seq)  # set so easier to check
     for val in seq:
@@ -52,15 +52,15 @@ for seq in sequences:
         # Get preceeding values based on rules
         vals_to_preceed_val = set(rules_dict[val])
         # Check if any unchecked vals is contained
-        violates = not vals_checked.isdisjoint(vals_to_preceed_val)
+        VIOLATES = not vals_checked.isdisjoint(vals_to_preceed_val)
 
-        if violates:
+        if VIOLATES:
             # For task 5.2
             incorrect_sequences.append(seq)
             # Don't need to check remaining sequence elements
             break
     # If loop was terminated via break, sequence doesn't get added
-    if not violates:
+    if not VIOLATES:
         correct_sequences.append(seq)
 
 print(f"Number of correct sequences {len(correct_sequences)}")
@@ -72,37 +72,75 @@ print(f"Sum of all middle pages: {sum(middle_pages)}")
 ## Task 5.2
 # We essentially repeat the approach, but now if a violation is detected, we
 # use intersection to put the values before the current value, resulting in the
-# correct order
+# correct order for the checked value. To ensure that the reordered values are in right
+# order, we repeat the process until all sequences are orderd
+# Note: This assumes that the order rules produce a unique sequence when correctly sorted
+# but otherwise it would also be difficult to check using middle pages. Wondering whether
+# there is a direct approach given this information..
+# Turns out there is https://en.wikipedia.org/wiki/Topological_sorting
 corrected_sequences = []
-for seq in sequences:
-    violates = False
+while len(incorrect_sequences) > 0:
+    # Differently to the previous loop we reorder one sequence
+    # at a time until it can be considered correctly ordered
+    seq_to_reorder_org = incorrect_sequences[0]
+    seq_to_reorder = seq_to_reorder_org.copy()  # placeholder for violates while
+    VIOLATES = True
+    reordered_sequence = seq_to_reorder_org.copy()
 
-    # to keep track which vals where already checked
-    vals_checked = set(seq)  # set so easier to check
+    while VIOLATES:
+        VIOLATES = False
+        # to keep track which vals where already checked
+        vals_checked = seq_to_reorder.copy()
 
-    # placeholder for reordered sequence
-    reordered_sequence = []
-    for val in seq:
-        # Add value to reordered sequence
-        # If values should preceed, they will be added before this
+        for val in seq_to_reorder:
+            # If values should preceed, they will be added before this
+            vals_checked.remove(val)
 
-        vals_checked.remove(val)
+            # This can only be executed if sequence is in correct order
+            if len(vals_checked) == 0:
+                VIOLATES = False
+                continue
 
-        # If no elements would be left, all are correct
-        if len(vals_checked) == 0:
-            continue
-        # Get preceeding values based on rules
-        vals_to_preceed_val = set(rules_dict[val])
+            # Get preceeding values based on rules
+            vals_to_preceed_val = set(rules_dict[val])
 
-        # If the value is correct, we can proceed to the next value
-        violates = not vals_checked.isdisjoint(vals_to_preceed_val)
+            # If the value is correct, we can proceed to the next value
+            VIOLATES = not set(vals_checked).isdisjoint(vals_to_preceed_val)
 
-        if violates:
-            # Get values which should preceed current one
-            should_preceed = vals_checked.intersection(vals_to_preceed_val)
+            if VIOLATES:
+                # Get first value which should preceed current one even if there are multiple
+                # except those are all the same
+                vals_should_preceed = list(
+                    set(vals_checked).intersection(vals_to_preceed_val)
+                )
+                vals_should_preceed = [
+                    v for v in vals_to_preceed_val if v == vals_should_preceed[0]
+                ]
 
-        # Manipulate sequence by first removing them and then inserting them
+                # remove first all occurences of the value
+                # and insert before currently checked value
+                for v in vals_should_preceed:
+                    reordered_sequence.remove(v)
+                cur_val_index = reordered_sequence.index(val)
 
-    # If loop was terminated via break, sequence doesn't get added
-    if not violates:
-        corrected_sequences.append(seq)
+                for v in vals_should_preceed:
+                    reordered_sequence.insert(cur_val_index, v)
+
+                # Don't need to check remaining sequence elements
+                break
+            else:
+                # Val must be in correct order already go to next
+                continue
+
+        # This is only executed after a reorder or if sequence is correct
+        if not VIOLATES:
+            corrected_sequences.append(reordered_sequence)
+            incorrect_sequences.remove(seq_to_reorder_org)
+        else:
+            # else means next reorder attempt is started
+            seq_to_reorder = reordered_sequence.copy()
+
+
+print(f"Number of corrected sequences {len(corrected_sequences)}")
+middle_pages = [seq[int((len(seq) - 1) / 2)] for seq in corrected_sequences]
+print(f"Sum of all middle pages for corrected sequences: {sum(middle_pages)}")
